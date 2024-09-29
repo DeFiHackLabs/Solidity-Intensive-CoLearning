dbf16ff83da0a1d7d439a8b88bd42ac5acca8e03
llt <1373872039@qq.com>
2024-09-29 18:27:17 +0800

    feat: update day7 note

---
+++ content/llt/src/106/1.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract HelloWorld {
+    address public owner; // 定义owner变量
+
+    // // 构造函数
+    constructor(address initialOwner) {
+        owner = initialOwner; // 在部署合约的时候，将owner设置为传入的initialOwner地址
+    }
+
+    // 定义modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner); // 检查调用者是否为owner地址
+        _; // 如果是的话，继续运行函数主体；否则报错并revert交易
+    }
+
+    function changeOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner; // 只有owner地址运行这个函数，并改变owner
+    }
+}
